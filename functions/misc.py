208660ac7baaef46633abc1d2dd78812e7688d9f
Clemence Herny <clemence.herny@gmail.com>
2023-09-27 16:46:59 +0000

    Add functions scripts

---
+++ functions/misc.py
@@ -0,0 +1,27 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+
+# import warnings
+# warnings.simplefilter(action='ignore', category=FutureWarning)
+
+# import os
+import sys
+# import geopandas as gpd
+
+# from shapely.affinity import scale
+# from rasterio.transform import from_bounds
+
+
+def format_logger(logger):
+
+    logger.remove()
+    logger.add(sys.stdout, format="{time:YYYY-MM-DD HH:mm:ss} - {level} - {message}",
+            level="INFO", filter=lambda record: record["level"].no < 25)
+    logger.add(sys.stdout, format="{time:YYYY-MM-DD HH:mm:ss} - <green>{level}</green> - {message}",
+            level="SUCCESS", filter=lambda record: record["level"].no < 30)
+    logger.add(sys.stdout, format="{time:YYYY-MM-DD HH:mm:ss} - <yellow>{level}</yellow> - {message}",
+            level="WARNING", filter=lambda record: record["level"].no < 40)
+    logger.add(sys.stderr, format="{time:YYYY-MM-DD HH:mm:ss} - <red>{level}</red> - <level>{message}</level>",
+            level="ERROR")
+    
+    return logger
