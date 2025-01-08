396ccc3e0131d5a054f45a8c083d07ca629f0811
Clemence Herny <clemence.herny@gmail.com>
2025-01-08 15:40:37 +0000

    Add mask building script

---
+++ scripts/mask_building.py
@@ -0,0 +1,109 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import time
+import argparse
+import yaml
+from glob import glob
+from tqdm import tqdm
+
+import geopandas as gpd
+import numpy as np
+import rasterio
+from rasterio.mask import mask
+from rasterio.features import rasterize
+from shapely.geometry import Polygon, mapping
+
+sys.path.insert(0, '.')
+import functions.misc as misc
+
+from loguru import logger
+logger = misc.format_logger(logger)
+
+# Define functions ------------------------------
+
+def poly_from_utm(polygon, transform):
+    poly_pts = []
+    
+    for i in np.array(polygon.exterior.coords):
+        
+        # Convert polygons to the image CRS
+        poly_pts.append(~transform * tuple(i))
+        
+    # Generate a polygon object
+    new_poly = Polygon(poly_pts)
+    return new_poly
+
+
+if __name__ == "__main__":
+
+    # Start chronometer
+    tic = time.time()
+    logger.info('Starting...')
+
+    # Argument and parameter specification
+    parser = argparse.ArgumentParser(description="The script prepares the ground truth dataset to be processed by the object-detector scripts")
+    parser.add_argument('config_file', type=str, help='Framework configuration file')
+    args = parser.parse_args()
+
+    logger.info(f"Using {args.config_file} as config file.")
+ 
+    with open(args.config_file) as fp:
+        cfg = yaml.load(fp, Loader=yaml.FullLoader)[os.path.basename(__file__)]
+
+    WORKING_DIR = cfg['working_dir']
+    ROOFS_SHP = cfg['roofs_shp']
+    IMAGE_FOLDER = cfg['image_dir']
+    TRANSPARENCY = cfg['transparency']
+
+    os.chdir(WORKING_DIR)
+
+    logger.info('Import data...')
+    roofs = gpd.read_file(ROOFS_SHP)
+    tiles = glob(os.path.join(IMAGE_FOLDER, '*.tif'))
+
+    if roofs.crs != 'epsg:3857':
+        roofs = roofs.to_crs(epsg=3857)
+
+    logger.info('Process vector data...')
+    roofs = roofs.buffer(0)
+    merged_roofs_geoms = roofs.unary_union
+
+    for tile in tqdm(tiles, desc='Produce masks', total=len(tiles)):
+
+        if TRANSPARENCY:
+            geoms_list = [mapping(merged_roofs_geoms)]
+
+            with rasterio.open(tile) as src:
+                mask_image, mask_transform = mask(src, geoms_list)
+                mask_meta = src.meta
+
+            mask_meta.update({'transform': mask_transform})
+            filepath = os.path.join(misc.ensure_dir_exists(os.path.join(IMAGE_FOLDER, 'masked_images')),
+                                os.path.splitext(os.path.basename(tile))[0] + '.tif')
+            
+            with rasterio.open(filepath, 'w', **mask_meta) as dst:
+                dst.write(mask_image)
+
+        else:
+            with rasterio.open(tile, "r") as src:
+                tile_img = src.read()
+                tile_meta = src.meta
+
+            im_size = (tile_meta['height'], tile_meta['width'])
+
+            polygons = [poly_from_utm(geom, src.meta['transform']) for geom in merged_roofs_geoms.geoms]
+            mask_image = rasterize(shapes=polygons, out_shape=im_size)
+
+            mask_meta = src.meta.copy()
+            mask_meta.update({'count': 1, 'dtype': 'uint8', 'nodata': 99})
+
+            filepath = os.path.join(misc.ensure_dir_exists(os.path.join(IMAGE_FOLDER, 'mask')),
+                                    os.path.splitext(os.path.basename(tile))[0] + '.tif')    
+
+            with rasterio.open(filepath, 'w', **mask_meta) as dst:
+                dst.write(mask_image, 1)
+
+    logger.success(f'The masks were written in the folder {os.path.join(IMAGE_FOLDER, "mask")}.')
